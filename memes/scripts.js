7781cef35491d82e2167f95f975f8f82b0723cff
SpUnity <spursunity@gmail.com>
2019-04-11 10:24:06 +0300

    2 attempt

--- memes/scripts.js
+++ memes/scripts.js
@@ -2,14 +2,4 @@
 
 window.onload = function () {
 	const btn = document.querySelector('#auth');
-
-	btn.onclick = function () {
-		fetch('https://oauth.vk.com/authorize?client_id=6939619&display=page&redirect_uri=http://spunity.github.io/memes/&display=popup&scope=friends&response_type=token&v=5.93&state=123456')
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((err) => {
-				console.log(err);
-			})
-	}
 }
